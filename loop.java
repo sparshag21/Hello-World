8c64b2656854f35ba78d9f1d054aff7798c4a783
Aditya Joardar <30207466+joardar-aditya@users.noreply.github.com>
2019-10-01 19:38:52 +0530

    Delete loop.java

--- loop.java
+++
@@ -1,9 +0,0 @@
-public class loop {
-    public static void main(String[] java){
-        String base = "* ";
-        for(int i=1; i<5; i++){
-            System.out.println(base);
-            base += "* ";
-        }
-    }
-}
