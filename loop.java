44dec4bd3fed3e197b0abd71b7c352654a9e47a4
Aditya Joardar <30207466+joardar-aditya@users.noreply.github.com>
2019-10-01 18:26:39 +0530

    new loop - with spaces

--- loop.java
+++ loop.java
@@ -1,8 +1,9 @@
 public class loop {
     public static void main(String[] java){
+        String base = "* ";
         for(int i=1; i<5; i++){
-            String print = new String(new char[i]).replace('\0','*');
-            System.out.println(print);
+            System.out.println(base);
+            base += "* ";
         }
     }
 }
