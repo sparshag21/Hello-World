fb48b2f3d29109d4c1275d7af9c640601ae264ac
Aditya Joardar <30207466+joardar-aditya@users.noreply.github.com>
2019-10-01 19:32:40 +0530

    Delete nameinput.java

--- nameinput.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class nameinput {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
-        System.out.println("Welcome! "+ name);
-    }
-}
