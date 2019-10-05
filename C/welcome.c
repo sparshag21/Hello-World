41459086ce5f06051eadae596768945fe3c9c290
Utkarsh Nagpure <41725470+Utkarsh2604@users.noreply.github.com>
2019-10-05 06:02:36 +0530

    Create welcome.c

---
+++ C/welcome.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main () 
+{
+  
+char name[20];
+
+printf ("\n Enter name: ");
+  
+scanf ("%s", name);
+  
+printf ("\n Your name is %s.\n ", name);
+
+printf("\n Welcome To Hacktoberfest");
+
+printf("\n Happy hacking");
+  
+return 0;
+
+}
